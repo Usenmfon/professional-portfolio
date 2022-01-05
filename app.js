7939154c7535d94775809b81788dafeff5773629
Usenmfon <usenmfonuko@gmail.com>
2022-01-05 12:09:59 +0100

    updated UI

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const openbtn = document.getElementById('openbtn')
-const links = document.getElementById('links')
-
 
 openbtn.addEventListener('click', ()=> {
-   
-    links.style.display = "flex"
+    var links = document.getElementById('links')
+    if(links.style.display == "none" ){
+
+        links.style.display = "flex"
+    } else {
+        links.style.display = "none"
+    }
 })
